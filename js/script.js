ff9687f1786a3238c73cd6dbce2e855da30deb16
leneind221 <ethan.leneindre@etu.unicaen.fr>
2023-09-15 15:37:09 +0200

    Fonctionnel sans changement de mode

--- js/script.js
+++ js/script.js
@@ -1,17 +1,78 @@
 // Define vars
 let workingState = true;
-let pauseButton = true;
-let workSeconds = 0;
-let workMinutes = 25;
-let breakSeconds = 0;
-let breakMinutes = 5;
+let resetButtonState = true;
 let HTMLTimer = document.getElementById("time");
 let progression = document.getElementById("progression");
-let title= document.getElementById("title");
+let title = document.getElementById("title");
+let workButton = document.getElementById("work");
+let breakButton = document.getElementById("break");
+let startButton = document.getElementById("start");
+let resetButton = document.getElementById("reset");
+let breakTime = document.getElementById("numberBreak");
+let workTime = document.getElementById("numberWork");
+let workSeconds = 0;
+let workMinutes = parseInt(workTime.getAttribute("value"));
+let breakSeconds = 0;
+let breakMinutes = parseInt(breakTime.getAttribute("value"));
+let interval;
 
-interval = setInterval(() => {
-    countDown();
-}, 100);
+// Init page
+breakButton.setAttribute("disabled", true);
+resetButton.style.display = "none";
+
+breakTime.addEventListener("change", () => {
+    breakMinutes = breakTime.value;
+})
+
+workTime.addEventListener("change", () => {
+    workMinutes = workTime.value;
+})
+
+startButton.addEventListener("click", () => {
+    resetButtonState = true;
+    startButton.style.display = "none";
+    resetButton.style.display = "block";
+    workButton.setAttribute("disabled", true);
+    breakButton.setAttribute("disabled", true);
+    manipInterval();
+});
+
+resetButton.addEventListener("click", () => {
+    resetButtonState = false;
+    manipInterval();
+    startButton.style.display = "block";
+    resetButton.style.display = "none";
+    if (workingState) {
+        workButton.setAttribute("disabled", false);
+    }
+    else {
+        breakButton.setAttribute("disabled", false);
+    }
+    workSeconds= 0;
+    breakSeconds= 0;
+    workMinutes = workTime.value;
+    breakMinutes = breakTime.value;
+    workingState ? timerDisplay(workSeconds, workMinutes) : timerDisplay(breakSeconds, breakMinutes)
+});
+
+workButton.addEventListener("click", () => {
+
+});
+
+breakButton.addEventListener("click", () => {
+
+});
+
+function manipInterval() {
+    if (resetButtonState) {
+        interval = setInterval(() => {
+            countDown();
+        }, 10);
+    }
+    else {
+        clearInterval(interval);
+    }
+}
 
 function countDown() {
     // Function which launches the countdown
@@ -38,13 +99,13 @@ function countDown() {
 
     // Reset timer, and switch the mode at the end of the timer
     if (workMinutes == 0 && workSeconds == 0) {
-        workMinutes = 25;
+        workMinutes = workTime;
         workSeconds = 0;
         timerDisplay(workSeconds, workMinutes);
         workingState = !workingState;
     }
     if (breakMinutes == 0 && breakSeconds == 0) {
-        breakMinutes = 5;
+        breakMinutes = breakTime;
         breakSeconds = 0;
         timerDisplay(workSeconds, workMinutes);
         workingState = !workingState;
@@ -75,12 +136,13 @@ function timerDisplay(seconds, minutes) {
     // Function which displays the timer in the HTML document + progression bar
     HTMLTimer.innerHTML = display(minutes) + ":" + display(seconds);
     // title.innerHTML.replace("$time", display(minutes) + ":" + display(seconds))
-    progression.setAttribute('value', percCalc(seconds, minutes));
+    let percent = percCalc();
+    progression.setAttribute("value", percent);
 }
 
-function percCalc(seconds, minutes){
-    if(workingState){
-        return timeToMS(seconds, minutes)*100/timeToMS(workSeconds, workMinutes);
+function percCalc() {
+    if (workingState) {
+        return timeToMS(workSeconds, workMinutes) * 100 / timeToMS(0, workTime);
     }
-    return timeToMS(seconds, minutes)*100/timeToMS(breakSeconds, breakMinutes);
+    return timeToMS(breakSeconds, breakMinutes) * 100 / timeToMS(0, breakTime);
 }
