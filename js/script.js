70ab92de3f3297b3b7045083ed9f0836124e78a7
leneind221 <ethan.leneindre@etu.unicaen.fr>
2023-09-15 14:11:40 +0200

    Avancement sur la barre de progression

--- js/script.js
+++ js/script.js
@@ -3,39 +3,40 @@ let workingState = true;
 let pauseButton = true;
 let workSeconds = 0;
 let workMinutes = 25;
-let workCountdown = timeToMS(workMinutes, workSeconds);
 let breakSeconds = 0;
 let breakMinutes = 5;
-let breakCountdown = timeToMS(breakMinutes, breakSeconds);
-let HTMLTimer= document.getElementById("time");
-let progression= document.getElementById("progression");
+let HTMLTimer = document.getElementById("time");
+let progression = document.getElementById("progression");
+let title= document.getElementById("title");
 
 interval = setInterval(() => {
     countDown();
-}, 1000);
+}, 100);
 
 function countDown() {
+    // Function which launches the countdown
     let seconds, minutes;
+    // Calculate each case (if you are in break mode or not, and if you are at the end of a minute)
     if (workingState && workSeconds <= 0) {
         workMinutes--;
         workSeconds = 59;
         timerDisplay(workSeconds, workMinutes);
     }
-    else if (!workingState && breakSeconds <= 0){
+    else if (!workingState && breakSeconds <= 0) {
         breakMinutes--;
         breakSeconds = 59;
         timerDisplay(breakSeconds, breakMinutes);
     }
-    else if(workingState && workSeconds> 0){
+    else if (workingState && workSeconds > 0) {
         workSeconds--;
         timerDisplay(workSeconds, workMinutes);
     }
-    else{
+    else {
         breakSeconds--;
         timerDisplay(breakSeconds, breakMinutes);
     }
-    
 
+    // Reset timer, and switch the mode at the end of the timer
     if (workMinutes == 0 && workSeconds == 0) {
         workMinutes = 25;
         workSeconds = 0;
@@ -50,29 +51,36 @@ function countDown() {
     }
 }
 
-function timeToMS(minutes, seconds) {
-    // Fonction pour convertir minutes et secondes en millisecondes
+function timeToMS(seconds, minutes) {
+    // Function to convert sec and min in ms
     return minutes * 60 * 1000 + seconds * 1000;
 }
 
 function MSToTime(milliseconds) {
-    // Fonction pour convertir millisecondes en minutes et secondes
+    // Function to convert ms to min and sec
     const totalSeconds = Math.floor(milliseconds / 1000);
     const minutes = Math.floor(totalSeconds / 60);
     const seconds = totalSeconds % 60;
-    return { minutes, seconds };
+    return { seconds, minutes };
 }
 
 function display(time) {
+    // Function which load the display, with a 0 added if it's necessary
     let StrTime = time.toString();
     StrTime = time < 10 ? "0" + StrTime : StrTime;
     return StrTime;
 }
 
 function timerDisplay(seconds, minutes) {
-    HTMLTimer.innerHTML = display(minutes)+":"+display(seconds);
-    
-    progression.innerHTML = ;
-    progression.setAttribute();
-    progression.setAttribute();
+    // Function which displays the timer in the HTML document + progression bar
+    HTMLTimer.innerHTML = display(minutes) + ":" + display(seconds);
+    // title.innerHTML.replace("$time", display(minutes) + ":" + display(seconds))
+    progression.setAttribute('value', percCalc(seconds, minutes));
+}
+
+function percCalc(seconds, minutes){
+    if(workingState){
+        return timeToMS(seconds, minutes)*100/timeToMS(workSeconds, workMinutes);
+    }
+    return timeToMS(seconds, minutes)*100/timeToMS(breakSeconds, breakMinutes);
 }
